4a4dd43dd6891680fff6d5067565cf3e521a334e
Juliandeplano <juliandeplano@tamu.edu>
2023-02-06 03:01:20 +0000

    made a function to download the log file

--- src/fparse.py
+++ src/fparse.py
@@ -1,8 +1,8 @@
 import requests
 
+def log_download():
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log' #url we are downloading from
+    r = requests.get(url, allow_redirects=True)
+    print(r.headers.get('content-type'))
 
-url = 'https://s3.amazonaws.com/tcmg476/http_access_log' #url we are downloading from
-r = requests.get(url, allow_redirects=True)
-print(r.headers.get('content-type'))
-
-open('log', 'wb').write(r.content)
+    open('log', 'wb').write(r.content)
