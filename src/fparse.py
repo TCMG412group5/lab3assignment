f690fcefa17d11bead08cb6c1f44037cd4b839d1
Juliandeplano <juliandeplano@tamu.edu>
2023-02-06 03:15:54 +0000

    added function to download and save http log file

--- src/fparse.py
+++ src/fparse.py
@@ -1,8 +1,6 @@
-import requests
+import urllib.request
 
-def log_download():
-    url = 'https://s3.amazonaws.com/tcmg476/http_access_log' #url we are downloading from
-    r = requests.get(url, allow_redirects=True)
-    print(r.headers.get('content-type'))
 
-    open('log', 'wb').write(r.content)
+def download_log():
+    urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "http_access_log")
+
