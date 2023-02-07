6865240a4fd03b18414f41333af40345d2e60d1e
romanc24 <123586433+romanc24@users.noreply.github.com>
2023-02-07 12:36:54 -0600

    Update Countentries.py

--- src/lab3/Countentries.py
+++ src/lab3/Countentries.py
@@ -3,3 +3,12 @@ with open(r"src/lab3/http_access_log", 'r') as fp:
     for count, line in enumerate(fp):
         pass
 print('Total Lines', count + 1)
+
+file = open('httplogs.txt', 'r')
+text = file.read()
+
+#count the requests from the time period log
+from dateutil import parser
+date = parser.parse("10th of April, 1995")
+date
+print(matches)
