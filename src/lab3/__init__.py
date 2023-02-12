591e4ab309b9263d3d4cf60f507ed6a338d249c6
Mattic <50807277+ImMattic@users.noreply.github.com>
2023-02-11 23:02:17 -0600

    Added .gitignore and __init__.py files (#15)
    * Added __init__.py file
    
    * added __init__.py and .gitignore
    
    * updated .gitignore and removed cached files
    
    ---------
    
    Co-authored-by: Matt <jacobie4@gmail.com>

---
+++ src/lab3/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package provides a module for parsing log files using regular expressions.
+
+The `regex_parse` module is the only module that is importable from this package.
+
+Example:
+    >>> from my_package import regex_parse
+"""
+__all__ = ["regex_parse"]
