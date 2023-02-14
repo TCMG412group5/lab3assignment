3d2b0bc65ae2e4aa9fbc14b494a8751225ea016d
leonelsortols <123771044+leonelsortols@users.noreply.github.com>
2023-02-13 23:47:48 -0600

    Leonel branch (#27)
    * Create month requests.py
    
    * Update month requests.py

---
+++ src/lab3/month requests.py
@@ -0,0 +1,21 @@
+import re
+import os
+from regex_parse import http_parse
+from log_reader import read_log
+log_lines = read_log("http_access_log")
+
+things  = {}
+for i in log_lines:
+    try:
+        log_dict = http_parse(i)
+        filename = log_dict['month']+ " "+log_dict['year']
+        if filename in things:
+            things[filename] += 1
+        else:
+            things[filename] = 1
+     #   print(filename, filename2, filename3)
+    except:
+        pass
+    
+for z in things:
+    print("On the month of ", z, "there were ",things[z], " entries")
