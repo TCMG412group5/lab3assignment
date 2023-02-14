90cc276d7039d125940bb37bb97746d27bb42cbc
Mattic <50807277+ImMattic@users.noreply.github.com>
2023-02-14 00:45:24 -0600

    added weekbyweek.py (#28)
    Co-authored-by: Matt <jacobie4@gmail.com>

--- src/lab3/month requests.py
+++ src/lab3/month requests.py
@@ -6,16 +6,13 @@ log_lines = read_log("http_access_log")
 
 things  = {}
 for i in log_lines:
-    try:
-        log_dict = http_parse(i)
-        filename = log_dict['month']+ " "+log_dict['year']
-        if filename in things:
-            things[filename] += 1
-        else:
-            things[filename] = 1
-     #   print(filename, filename2, filename3)
-    except:
-        pass
+    log_dict = http_parse(i)
+    filename = log_dict['month']+ " "+log_dict['year']
+    if filename in things:
+        things[filename] += 1
+    else:
+        things[filename] = 1
+    #   print(filename, filename2, filename3)
     
 for z in things:
     print("On the month of ", z, "there were ",things[z], " entries")
